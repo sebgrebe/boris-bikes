a3b29542f3af12e555ad0222c647cc2afaf9a55b
Patrick Harris <patricknpharris@gmail.com>
2018-09-04 17:27:02 +0100

    currently in chapter 11, various tests with other code commented out (saved for Alex to reread later

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,19 @@
 require 'docking_station'
+
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'gives a working bike' do
     expect(subject.release_bike).to be_working
   end
+
+  # it { is_expected.to respond_to :dock_bike }
+
+  it { is_expected.to respond_to :dock_bike }
+
+  # it '...' do
+  #   bike = Bike.new
+  #   expect(subject.dock_bike(bike)).to eq('bike')
+  # end
 
 end
